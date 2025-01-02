78eb92dec88ca492b8ba0e3a9585e50837b9fbbe
Kaufko <99084056+Kaufko@users.noreply.github.com>
2025-01-02 16:53:05 +0100

    Upgraded sudo
    now has 2 modes
    - non input
    - input
    
    non input just elevates CMD
    input elevates cmd and passes argument as command

--- sudo.bat
+++ sudo.bat
@@ -1,1 +1,30 @@
-powershell.exe -Command "Start-Process cmd \"/k cd /d %cd%\" -Verb RunAs"
+@echo off
+
+set args=
+
+if "%1" == "test" goto :hasAdmin
+
+:getadmin
+    REM elevates CMD
+    ECHO Requesting administrative privileges...
+    PING -n 2 127.0.0.1 >NUL
+    PowerShell -Command "Start-Process cmd.exe -ArgumentList '/k cd /d \"%CD%\" && \"%~f0\" test %*' -Verb RunAs"
+    EXIT
+
+:hasAdmin
+    if "%2" == "" goto :noInput
+    if not "%2" == "" goto :collectArgs
+
+:noInput
+    EXIT /B
+
+:hasArgs
+    call %args%
+    exit /B
+
+:collectArgs
+    echo "%~1"
+    shift
+    if "%~1"=="" goto :hasArgs
+    set args=%args% %~1
+    goto :collectArgs
